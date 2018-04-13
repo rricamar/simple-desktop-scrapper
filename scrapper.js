a04435f84e13ffe73f060da39788f18b3719462f
rricamar <r.ricamarti@gmail.com>
2018-04-13 18:56:56 +0200

    Apply 4 tab size to all repository

--- scrapper.js
+++ scrapper.js
@@ -8,70 +8,75 @@ const log = console.log;
 const UrlHelper = require('./url');
 
 class Scrapper {
-  constructor() {
-    this.request = request;
-    this.cheerio = cheerio;
-    this.urlHelper = new UrlHelper();
-  }
-
-  async start() {
-    log(chalk.blue('Starting...'));
-    const body = await this.get(this.urlHelper.browseUrl);
-    const html = this.parse(body);
-    const imagesUrls = this.getImages(html);
-    const binary = {
-      encoding: 'binary'
-    };
-    const rawImages = [];
-    for (var i = 0; i < imagesUrls.length; i++) {
-      const imageUrl = imagesUrls[i];
-      const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
-      const image = await this.get(fixedImageUrl, binary);
-      const filename = uid(20) + '.png';
-      this.save(filename, image);
+    constructor() {
+        this.request = request;
+        this.cheerio = cheerio;
+        this.urlHelper = new UrlHelper();
     }
-  }
 
-  async get(url, options = {}) {
-    log(chalk.blue(`Getting ${url} ...`));
-    try {
-      const body = await request(url, options);
-      log(chalk.blue(`... done!`));
-      return body;
-    } catch (err) {
-      this.onError(err);
+    async start() {
+        log(chalk.blue('Starting...'));
+        const body = await this.get(this.urlHelper.browseUrl);
+        const html = this.parse(body);
+        const imagesUrls = this.getImages(html);
+        const binary = {
+            encoding: 'binary'
+        };
+
+        const rawImages = [];
+
+        for (var i = 0; i < imagesUrls.length; i++) {
+            const imageUrl = imagesUrls[i];
+            const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
+            const image = await this.get(fixedImageUrl, binary);
+            const filename = uid(20) + '.png';
+            this.save(filename, image);
+        }
     }
-  }
 
-  parse(html) {
-    return this.cheerio.load(html);
-  }
+    async get(url, options = {}) {
+        log(chalk.blue(`Getting ${url} ...`));
 
-  save(filename, content) {
-    const dir = './images';
-    const base = filename;
-    const pathFile = path.format({
-      dir,
-      base
-    });
-    log(chalk.green(`Saving ${pathFile} ...`));
-    try {
-      fs.writeFileSync(pathFile, content, 'binary');
-      log(chalk.green(`... done!`));
-    } catch (err) {
-      this.onError(err);
+        try {
+            const body = await request(url, options);
+            log(chalk.blue(`... done!`));
+            return body;
+        } catch (err) {
+            this.onError(err);
+        }
     }
-  }
 
-  getImages(html) {
-    const images = html('body .container .desktop img');
-    const imagesUrls = images.map((index, elem) => elem.attribs.src).toArray();
-    return imagesUrls;
-  }
+    parse(html) {
+        return this.cheerio.load(html);
+    }
 
-  onError(err) {
-    log(chalk.red(err));
-  }
+    save(filename, content) {
+        const dir = './images';
+        const base = filename;
+        const pathFile = path.format({
+            dir,
+            base
+        });
+
+        log(chalk.green(`Saving ${pathFile} ...`));
+
+        try {
+            fs.writeFileSync(pathFile, content, 'binary');
+            log(chalk.green(`... done!`));
+        } catch (err) {
+            this.onError(err);
+        }
+    }
+
+    getImages(html) {
+        const images = html('body .container .desktop img');
+        const imagesUrls = images.map((index, elem) => elem.attribs.src).toArray();
+        return imagesUrls;
+    }
+
+    onError(err) {
+        log(chalk.red(err));
+    }
 }
 
 module.exports = Scrapper;
