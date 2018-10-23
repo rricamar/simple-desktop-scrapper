8c2c3eed2ef05fe1f3b8685a3c187d2c83d68f48
Pablo <psirera4@gmail.com>
2018-10-23 22:54:24 +0200

    Add funcionality that create dir when it does not exists

--- scrapper.js
+++ scrapper.js
@@ -1,82 +1,85 @@
-const fs = require('fs');
-const path = require('path');
-const request = require('request-promise-native');
-const cheerio = require('cheerio');
-const chalk = require('chalk');
-const uid = require('uid');
+const fs = require("fs");
+const path = require("path");
+const request = require("request-promise-native");
+const cheerio = require("cheerio");
+const chalk = require("chalk");
+const uid = require("uid");
 const log = console.log;
-const UrlHelper = require('./url');
+const UrlHelper = require("./url");
 
 class Scrapper {
-    constructor() {
-        this.request = request;
-        this.cheerio = cheerio;
-        this.urlHelper = new UrlHelper();
+  constructor() {
+    this.request = request;
+    this.cheerio = cheerio;
+    this.urlHelper = new UrlHelper();
+  }
+
+  async start() {
+    log(chalk.blue("Starting..."));
+    const body = await this.get(this.urlHelper.browseUrl);
+    const html = this.parse(body);
+    const imagesUrls = this.getImages(html);
+    const binary = {
+      encoding: "binary"
+    };
+
+    const rawImages = [];
+
+    for (var i = 0; i < imagesUrls.length; i++) {
+      const imageUrl = imagesUrls[i];
+      const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
+      const image = await this.get(fixedImageUrl, binary);
+      const filename = uid(20) + ".png";
+      this.save(filename, image);
     }
+  }
 
-    async start() {
-        log(chalk.blue('Starting...'));
-        const body = await this.get(this.urlHelper.browseUrl);
-        const html = this.parse(body);
-        const imagesUrls = this.getImages(html);
-        const binary = {
-            encoding: 'binary'
-        };
+  async get(url, options = {}) {
+    log(chalk.blue(`Getting ${url} ...`));
 
-        const rawImages = [];
-
-        for (var i = 0; i < imagesUrls.length; i++) {
-            const imageUrl = imagesUrls[i];
-            const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
-            const image = await this.get(fixedImageUrl, binary);
-            const filename = uid(20) + '.png';
-            this.save(filename, image);
-        }
+    try {
+      const body = await request(url, options);
+      log(chalk.blue(`... done!`));
+      return body;
+    } catch (err) {
+      this.onError(err);
     }
+  }
 
-    async get(url, options = {}) {
-        log(chalk.blue(`Getting ${url} ...`));
+  parse(html) {
+    return this.cheerio.load(html);
+  }
 
-        try {
-            const body = await request(url, options);
-            log(chalk.blue(`... done!`));
-            return body;
-        } catch (err) {
-            this.onError(err);
-        }
+  save(filename, content) {
+    const dir = "./images";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
+    const base = filename;
+    const pathFile = path.format({
+      dir,
+      base
+    });
 
-    parse(html) {
-        return this.cheerio.load(html);
+    log(chalk.green(`Saving ${pathFile} ...`));
+
+    try {
+      fs.writeFileSync(pathFile, content, "binary");
+      log(chalk.green(`... done!`));
+    } catch (err) {
+      this.onError(err);
     }
+  }
 
-    save(filename, content) {
-        const dir = './images';
-        const base = filename;
-        const pathFile = path.format({
-            dir,
-            base
-        });
+  getImages(html) {
+    const images = html("body .container .desktop img");
+    const imagesUrls = images.map((index, elem) => elem.attribs.src).toArray();
+    return imagesUrls;
+  }
 
-        log(chalk.green(`Saving ${pathFile} ...`));
-
-        try {
-            fs.writeFileSync(pathFile, content, 'binary');
-            log(chalk.green(`... done!`));
-        } catch (err) {
-            this.onError(err);
-        }
-    }
-
-    getImages(html) {
-        const images = html('body .container .desktop img');
-        const imagesUrls = images.map((index, elem) => elem.attribs.src).toArray();
-        return imagesUrls;
-    }
-
-    onError(err) {
-        log(chalk.red(err));
-    }
+  onError(err) {
+    log(chalk.red(err));
+  }
 }
 
 module.exports = Scrapper;
--- url.js
+++ url.js
@@ -1,22 +1,19 @@
-const CONFIG = require('./config');
+const CONFIG = require("./config");
 
 class UrlHelper {
-    constructor() {
-    }
+  get mainUrl() {
+    return CONFIG.URL;
+  }
 
-    get mainUrl() {
-        return CONFIG.URL;
-    }
+  get browseUrl() {
+    return `${CONFIG.URL}/${CONFIG.BROWSE}`;
+  }
 
-    get browseUrl() {
-        return `${CONFIG.URL}/${CONFIG.BROWSE}`;
-    }
-
-    sanitizeImageUrl(url) {
-        const fullSizeUrl = url.split('.png')[0];
-        const fullSizeUrlWithExtension = `${fullSizeUrl}.png`;
-        return fullSizeUrlWithExtension;
-    }
+  sanitizeImageUrl(url) {
+    const fullSizeUrl = url.split(".png")[0];
+    const fullSizeUrlWithExtension = `${fullSizeUrl}.png`;
+    return fullSizeUrlWithExtension;
+  }
 }
 
 module.exports = UrlHelper;
