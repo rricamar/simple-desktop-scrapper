75e35b57483736896a79ccb798bc328f2e63ebe7
Pablo Sirera <psirera4@gmail.com>
2018-10-24 12:04:36 +0200

    Fix indentation

--- scrapper.js
+++ scrapper.js
@@ -8,78 +8,80 @@ const log = console.log;
 const UrlHelper = require('./url');
 
 class Scrapper {
-  constructor() {
-    this.request = request;
-    this.cheerio = cheerio;
-    this.urlHelper = new UrlHelper();
-  }
-
-  async start() {
-    log(chalk.blue('Starting...'));
-    const body = await this.get(this.urlHelper.browseUrl);
-    const html = this.parse(body);
-    const imagesUrls = this.getImages(html);
-    const binary = {
-      encoding: 'binary'
-    };
-
-    const rawImages = [];
-
-    for (var i = 0; i < imagesUrls.length; i++) {
-      const imageUrl = imagesUrls[i];
-      const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
-      const image = await this.get(fixedImageUrl, binary);
-      const filename = uid(20) + '.png';
-      this.save(filename, image);
+    constructor() {
+        this.request = request;
+        this.cheerio = cheerio;
+        this.urlHelper = new UrlHelper();
     }
-  }
 
-  async get(url, options = {}) {
-    log(chalk.blue(`Getting ${url} ...`));
+    async start() {
+        log(chalk.blue('Starting...'));
+        const body = await this.get(this.urlHelper.browseUrl);
+        const html = this.parse(body);
+        const imagesUrls = this.getImages(html);
+        const binary = {
+            encoding: 'binary'
+        };
 
-    try {
-      const body = await request(url, options);
-      log(chalk.blue(`... done!`));
-      return body;
-    } catch (err) {
-      this.onError(err);
+        const rawImages = [];
+
+        for (var i = 0; i < imagesUrls.length; i++) {
+            const imageUrl = imagesUrls[i];
+            const fixedImageUrl = this.urlHelper.sanitizeImageUrl(imageUrl);
+            const image = await this.get(fixedImageUrl, binary);
+            const filename = uid(20) + '.png';
+            this.save(filename, image);
+        }
     }
-  }
 
-  parse(html) {
-    return this.cheerio.load(html);
-  }
+    async get(url, options = {}) {
+        log(chalk.blue(`Getting ${url} ...`));
 
-  save(filename, content) {
-    const dir = './images';
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
+        try {
+            const body = await request(url, options);
+            log(chalk.blue(`... done!`));
+            return body;
+        } catch (err) {
+            this.onError(err);
+        }
     }
-    const base = filename;
-    const pathFile = path.format({
-      dir,
-      base
-    });
 
-    log(chalk.green(`Saving ${pathFile} ...`));
-
-    try {
-      fs.writeFileSync(pathFile, content, 'binary');
-      log(chalk.green(`... done!`));
-    } catch (err) {
-      this.onError(err);
+    parse(html) {
+        return this.cheerio.load(html);
     }
-  }
 
-  getImages(html) {
-    const images = html('body .container .desktop img');
-    const imagesUrls = images.map((index, elem) => elem.attribs.src).toArray();
-    return imagesUrls;
-  }
+    save(filename, content) {
+        const dir = './images';
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+        const base = filename;
+        const pathFile = path.format({
+            dir,
+            base
+        });
 
-  onError(err) {
-    log(chalk.red(err));
-  }
+        log(chalk.green(`Saving ${pathFile} ...`));
+
+        try {
+            fs.writeFileSync(pathFile, content, 'binary');
+            log(chalk.green(`... done!`));
+        } catch (err) {
+            this.onError(err);
+        }
+    }
+
+    getImages(html) {
+        const images = html('body .container .desktop img');
+        const imagesUrls = images
+            .map((index, elem) => elem.attribs.src)
+            .toArray();
+        return imagesUrls;
+    }
+
+    onError(err) {
+        log(chalk.red(err));
+    }
 }
 
 module.exports = Scrapper;
--- url.js
+++ url.js
@@ -1,19 +1,19 @@
 const CONFIG = require('./config');
 
 class UrlHelper {
-  get mainUrl() {
-    return CONFIG.URL;
-  }
+    get mainUrl() {
+        return CONFIG.URL;
+    }
 
-  get browseUrl() {
-    return `${CONFIG.URL}/${CONFIG.BROWSE}`;
-  }
+    get browseUrl() {
+        return `${CONFIG.URL}/${CONFIG.BROWSE}`;
+    }
 
-  sanitizeImageUrl(url) {
-    const fullSizeUrl = url.split('.png')[0];
-    const fullSizeUrlWithExtension = `${fullSizeUrl}.png`;
-    return fullSizeUrlWithExtension;
-  }
+    sanitizeImageUrl(url) {
+        const fullSizeUrl = url.split('.png')[0];
+        const fullSizeUrlWithExtension = `${fullSizeUrl}.png`;
+        return fullSizeUrlWithExtension;
+    }
 }
 
 module.exports = UrlHelper;
