379244b66140aff999777720344ff49b7f585496
Ruben Rica <r.ricamarti@gmail.com>
2019-10-02 19:27:36 +0200

    style: class convention

--- url.js
+++ url.js
@@ -1,19 +1,24 @@
 const CONFIG = require("./config")
 
+
 class UrlHelper {
+
     get mainUrl() {
         return CONFIG.URL
     }
 
+
     get browseUrl() {
         return `${CONFIG.URL}/${CONFIG.BROWSE}`
     }
+
 
     sanitizeImageUrl(url) {
         const fullSizeUrl = url.split(".png")[0]
         const fullSizeUrlWithExtension = `${fullSizeUrl}.png`
         return fullSizeUrlWithExtension
     }
+
 }
 
 module.exports = UrlHelper
