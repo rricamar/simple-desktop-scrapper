d62c39acc8c0c0014d6a542e3a93df2e469309d1
Ruben Rica <r.ricamarti@gmail.com>
2020-05-27 15:54:04 +0200

    fix: add jpeg extension

--- test/unit/url.test.js
+++ test/unit/url.test.js
@@ -9,9 +9,16 @@ test("should create", () => {
     expect(urlHelper).toBeDefined()
 })
 
-test("should return the url of the image at full resolution", () => {
-    const url = "http://static.simpledesktops.com/uploads/desktops/2019/06/22/Dinosaur_eye_2.png.295x184_q100.png"
-    const fullResUrl = "http://static.simpledesktops.com/uploads/desktops/2019/06/22/Dinosaur_eye_2.png"
+test("should return the url of the image at full resolution (PNG)", () => {
+    const url = "http://static.simpledesktops.com/uploads/desktops/2020/05/06/sphericalharmonics1.png.295x184_q100.png"
+    const fullResUrl = "http://static.simpledesktops.com/uploads/desktops/2020/05/06/sphericalharmonics1.png"
     const result = urlHelper.getFullResImageUrl(url)
-    expect(result).toBe(fullResUrl)
+    expect(result.fullSizeUrlWithExtension).toBe(fullResUrl)
+})
+
+test("should return the url of the image at full resolution (JPEG)", () => {
+    const url = "http://static.simpledesktops.com/uploads/desktops/2020/03/30/piano.jpg.295x184_q100.png"
+    const fullResUrl = "http://static.simpledesktops.com/uploads/desktops/2020/03/30/piano.jpg"
+    const result = urlHelper.getFullResImageUrl(url)
+    expect(result.fullSizeUrlWithExtension).toBe(fullResUrl)
 })
